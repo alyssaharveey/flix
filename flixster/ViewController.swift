935ac09235bc6ce2a4c22f0dec4e8d60b3acfe24
alyssaharveey <ajharvey@uci.edu>
2021-04-04 21:42:31 -0700

    Initial Commit

---
+++ flixster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  flixster
+//
+//  Created by Alyssa Harvey on 4/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
