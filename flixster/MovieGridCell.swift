e807c0bd77a1fd208f91c6917ee02defbbaac589
alyssaharveey <ajharvey@uci.edu>
2021-04-12 16:55:18 -0700

    Assignment 2

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Alyssa Harvey on 4/12/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
